9683a9fefb7d7cb4449910d71c5b99ac4b825630
i-am-tista <riddhita0508c@gmail.com>
2025-07-30 17:27:40 +0530

    Create DriverSetup.java

---
+++ src/test/java/driver/DriverSetup.java
@@ -0,0 +1,21 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverSetup {
+
+    public static WebDriver createWebDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        WebDriver driver = new ChromeDriver(options);
+        return driver;
+    }
+}
